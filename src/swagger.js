84afa952bec73d3818132592a87e35354e12a411
Danyllo VS <danyllo.dvs@gmail.com>
2025-07-15 00:16:36 -0300

    refactor: adusting heatlth check 2

--- src/swagger.js
+++ src/swagger.js
@@ -5,7 +5,7 @@ const options = {
     openapi: "3.0.0",
     info: {
       title: "API de Contas",
-      version: "1.0.1",
+      version: "1.0.2",
       description: "Documentação da API de Contas",
     },
     tags: [
