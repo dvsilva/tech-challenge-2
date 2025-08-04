7f010d6fb90f8df39b36421722c2ed4ac663e570
Danyllo VS <danyllo.dvs@gmail.com>
2025-08-04 19:27:14 -0300

    feat: add version

--- src/swagger.js
+++ src/swagger.js
@@ -5,7 +5,7 @@ const options = {
     openapi: "3.0.0",
     info: {
       title: "API de Contas",
-      version: "1.0.15",
+      version: "1.0.16",
       description: "Documentação da API de Contas",
     },
     tags: [
